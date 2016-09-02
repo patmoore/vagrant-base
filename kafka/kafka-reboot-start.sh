5694e83347f4acbac53cadf9279848a3edd97c68
Patrick Moore <patmoore@farreach.es>
2016-09-01 17:02:17 -0700

    https://wiki.debian.org/LSBInitScripts

--- kafka/kafka-reboot-start.sh
+++ kafka/kafka-reboot-start.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          scriptname
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start zookeeper and kafka at boot time
+# Description:       zookeeper and kafka
+### END INIT INFO
+# see https://wiki.debian.org/LSBInitScripts
+
 export KAFKA_HOME=/usr/local/kafka
 export PATH=$KAFKA_HOME/bin:$PATH
 export zookeeper_connect=localhost:2181
